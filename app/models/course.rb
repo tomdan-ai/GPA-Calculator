8660a033a5a76d80894111eaf9651a13327249ac
tomdan-ai <tomudoh258@gmail.com>
2023-11-09 02:09:25 +0100

    Add CGPA Logic

--- app/models/course.rb
+++ app/models/course.rb
@@ -20,5 +20,15 @@ class Course < ApplicationRecord
   
       (total_quality_points / total_credit_units).round(2)
     end
+
+    def self.calculate_cgpa(student_id)
+        student_courses = Course.where(student_id: student_id)
+        all_quality_points = student_courses.sum(&:calculate_quality_points)
+        all_credit_units = student_courses.sum(&:credit_unit)
+    
+        return 0.0 if all_credit_units.zero?
+    
+        (all_quality_points / all_credit_units).round(2)
+      end
   end
   
