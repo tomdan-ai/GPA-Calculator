0f889c23549a316c1581b5a3c7e2581b1f001c5a
tomdan-ai <tomudoh258@gmail.com>
2023-11-09 07:28:56 +0100

    Add logic to results controller

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,2 +1,14 @@
 class ResultsController < ApplicationController
-end
+    def semester_results
+      # Retrieve courses for a specific semester (adjust as needed)
+      @courses = Course.where(semester: params[:semester])
+      @gpa = Course.calculate_semester_gpa(@courses)
+    end
+  
+    def overall_results
+      # Retrieve all courses for the student (adjust as needed)
+      @all_courses = Course.where(student_id: current_user.id)
+      @cgpa = Course.calculate_cgpa(current_user.id)
+    end
+  end
+  
