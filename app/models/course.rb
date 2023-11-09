b073b46e8b5f1021bd1d74177b39d7a77093c1b3
tomdan-ai <tomudoh258@gmail.com>
2023-11-09 02:06:52 +0100

    Create Grade points in Models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,15 @@
 class Course < ApplicationRecord
-end
+    GRADE_POINTS = {
+      'A' => 5.00,
+      'B' => 4.00,
+      'C' => 3.00,
+      'D' => 2.00,
+      'E' => 1.00,
+      'F' => 0.00
+    }
+  
+    def calculate_quality_points
+      GRADE_POINTS[grade] * credit_unit
+    end
+  end
+  
